322d01b45134f882a05f44e0d777d84baf0cee1a
Ryan Poy <113697457+RyanPoyyy@users.noreply.github.com>
2023-08-31 11:02:35 +0800

    nice picture
    nice

--- src/components/sample/Rapie.tsx
+++ src/components/sample/Rapie.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
 export const Rapie = () => {
-  return <div>Rapie sucks dick</div>;
+  return (
+    <>
+      <div>Rapie sucks </div>
+      <div>
+        <img src="/src/assets/img/boolets.jpeg" alt="picture" />
+      </div>
+    </>
+  );
 };
 
 export default Rapie;
