d9effe9e5d8507b1c2878d7865dccbe1c87a0e5f
CCJH23 <clovischowjh@gmail.com>
2023-09-19 11:45:46 +0800

    Tidied up directories and added user service

---
+++ backend/src/main/java/g1t1/backend/BackendApplication.java
@@ -0,0 +1,39 @@
+package g1t1.backend;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(BackendApplication.class, args);
+
+		ClassLoader classLoader = BackendApplication.class.getClassLoader();
+
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+		FileInputStream serviceAccount = new FileInputStream(file.getAbsoluteFile());
+
+		FirebaseOptions.Builder builder = FirebaseOptions.builder();
+
+		FirebaseOptions options = builder.setCredentials(
+			GoogleCredentials.fromStream(serviceAccount)
+			)
+			.build();
+
+		FirebaseApp.initializeApp(options);
+
+		System.out.println("Connected to Firebase!");
+
+	}
+}
---
+++ backend/src/main/java/g1t1/backend/user/UserService.java
@@ -0,0 +1,119 @@
+package g1t1.backend.user;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.firebase.auth.UserRecord.UpdateRequest;
+
+@Service
+public class UserService {
+    
+    /**
+     * Description of the method: retrieve all user data using uid
+     *
+     * @param uid userId of user to retrieve
+     * @return return userRecord which is all the user data as an object
+     * @throws FirebaseAuthException Generic exception related to Firebase Authentication. Check the error code and message for more details.
+     */
+    public UserRecord getUser(@RequestParam String uid) {
+
+        UserRecord userRecord;
+
+        try {
+            userRecord = FirebaseAuth.getInstance().getUser(uid);
+
+            System.out.println("Successfully fetched user data: " + userRecord.getUid());
+    
+            return userRecord;
+
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            System.out.println("Failed to fetch user data: " + uid);
+        }
+        return null;
+    }
+
+
+    /**
+     * Description of the method: create user using email and password
+     *
+     * @param email email of user to create
+     * @param password password of user to create
+     * @return return userRecord which is all the user data as an object
+     * @throws FirebaseAuthException Generic exception related to Firebase Authentication. Check the error code and message for more details.
+     */
+    public UserRecord createUser(@RequestParam String email, @RequestParam String password) {
+
+        try {            
+            CreateRequest request = new CreateRequest()
+                .setEmail(email)
+                .setEmailVerified(false)
+                .setPassword(password)
+                .setDisabled(false);
+
+            UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+            System.out.println("Successfully created new user: " + userRecord.getUid());
+
+            return userRecord;
+
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            System.out.println("Failed to new user.");
+        }
+        return null;
+    }
+
+
+    /**
+     * Description of the method: edit user details using user details to update
+     *
+     * @param uid uid of user to edit
+     * @param email email of user to edit
+     * @param password password of user to edit
+     * @return return userRecord which is all the updated user data as an object
+     * @throws FirebaseAuthException Generic exception related to Firebase Authentication. Check the error code and message for more details.
+     */
+    public UserRecord editUser(@RequestParam String uid, @RequestParam String email, @RequestParam String password) {
+
+        try {
+            UpdateRequest request = new UpdateRequest(uid)
+                .setEmail(email)
+                .setPassword(password);
+
+            UserRecord userRecord = FirebaseAuth.getInstance().updateUser(request);
+            System.out.println("Successfully updated user: " + userRecord.getUid());
+
+            return userRecord;
+
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            System.out.println("Failed to update user.");
+        }
+        return null;
+    } 
+
+
+    /**
+     * Description of the method: delete user details using uid
+     *
+     * @param uid uid of user to edit
+     * @return do not return anything
+     * @throws FirebaseAuthException Generic exception related to Firebase Authentication. Check the error code and message for more details.
+     */
+    public void deleteUser(@RequestParam String uid) {
+
+        try {
+            FirebaseAuth.getInstance().deleteUser(uid);
+            System.out.println("Successfully deleted user.");
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            System.out.println("Failed to delete user.");
+        }
+    }
+
+}
+
