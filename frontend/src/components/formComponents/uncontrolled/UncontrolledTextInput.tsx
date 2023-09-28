8f720241aa65223309c795af6ce112b3b57c915a
Ryan Poy <113697457+RyanPoyyy@users.noreply.github.com>
2023-09-29 02:38:00 +0800

    created a controlled text input

---
+++ frontend/src/components/formComponents/uncontrolled/UncontrolledTextInput.tsx
@@ -0,0 +1,61 @@
+// INCOMPLETED ----------------------------------------------------------------
+
+import { Button, TextField } from "@mui/material";
+import React, { FC, useEffect, useState } from "react";
+
+interface Props {
+  label: string;
+  placeholder?: string;
+  formControlId: string;
+  formValue: string;
+  setFormControlState: React.Dispatch<React.SetStateAction<any>>;
+  error?: boolean;
+  errorText?: string;
+  style?: {};
+  className?: string;
+}
+
+const ControlledTextInput: FC<Props> = ({
+  label,
+  placeholder,
+  formControlId,
+  formValue,
+  setFormControlState,
+  error = false,
+  errorText,
+  style,
+  className,
+}) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormControlState({ ...FormData, [formControlId]: event.target.value });
+  };
+
+  //   const submitEvent
+
+  return (
+    <>
+      <TextField
+        multiline
+        label={label}
+        placeholder={placeholder}
+        error={
+          error &&
+          (formValue == "" || formValue == undefined || formValue == null)
+        }
+        helperText={
+          error &&
+          (formValue == "" || formValue == undefined || formValue == null)
+            ? errorText
+            : ""
+        }
+        value={formValue}
+        onChange={handleChange}
+        style={{ margin: "20px" }}
+      />
+    </>
+  );
+};
+
+export default ControlledTextInput;
--- frontend/src/routes/Screens.tsx
+++ frontend/src/routes/Screens.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import * as LINKS from "./links.ts";
 import Rapie from "../components/sample/Rapie";
 import Home from "../views/Home.tsx";
-import NavBar from "../components/NavBar/NavBar";
+import Testing from "../views/Testing.tsx";
 
 const Screens = () => {
   return (
     <Routes>
       <Route path={LINKS.HOME_PAGE} element={<Home />} />
       <Route path={LINKS.RAPIE_PAGE} element={<Rapie />} />
-      <Route path={LINKS.TESTING} element={<NavBar />} />
+      <Route path={LINKS.TESTING} element={<Testing />} />
     </Routes>
   );
 };
