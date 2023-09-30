d2e31770a3a049dd1356160233d25bdc1ef48366
CCJH23 <clovischowjh@gmail.com>
2023-09-30 17:00:59 +0800

    Added CORS for PortfolioController

--- backend/src/main/java/g1t1/backend/portfolio/PortfolioController.java
+++ backend/src/main/java/g1t1/backend/portfolio/PortfolioController.java
@@ -3,6 +3,7 @@ package g1t1.backend.portfolio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 @RequestMapping("/api/portfolio")
 public class PortfolioController {
