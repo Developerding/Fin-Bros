0924208037f21c38fef18da7237c07f175434c6f
anirbanter <jeettheman24@gmail.com>
2023-10-21 03:42:15 +0800

    View Portfolio Details
    updated the UI for /viewportfoliodetails and working on connecting the page to the mongo portfolio db

--- backend/src/main/java/g1t1/backend/portfolio/PortfolioController.java
+++ backend/src/main/java/g1t1/backend/portfolio/PortfolioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/portfolio")
+@CrossOrigin(origins="http://localhost:5173")
 public class PortfolioController {
     private final PortfolioService portfolioService;
 
--- frontend/src/stores/AppStore.ts
+++ frontend/src/stores/AppStore.ts
@@ -128,6 +128,24 @@ class AppStore {
       return err;
     }
   };
+
+  // // View Portfolio Details- from backend PortfolioController
+  // viewPortfolioController = async (portfolioName : string) => {
+  //   const headers = {
+  //     Cookie: 'USERID=1',
+  //   };
+  //   try {
+  //     const response = await axios.get(
+  //       `http://localhost:8080/api/portfolio/${portfolioName}`, {headers}
+  //       );      
+  //     console.log(response.data);
+  //     return response.data;
+  //   } catch (err) {
+  //     console.log(err);
+  //     return err;
+  //   }
+  // };
+
 }
 
 const hydrate = create({
