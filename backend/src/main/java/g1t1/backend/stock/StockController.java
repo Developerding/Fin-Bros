da80a05d938666627af3cf06579d3f1531adcb97
CCJH23 <clovischowjh@gmail.com>
2023-10-20 15:29:22 +0800

    Added API to call AlphaVantage API and store stock

--- backend/src/main/java/g1t1/backend/stock/StockController.java
+++ backend/src/main/java/g1t1/backend/stock/StockController.java
@@ -2,12 +2,13 @@ package g1t1.backend.stock;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import g1t1.backend.stock.StockRepository;
+// import g1t1.backend.stock.StockRepository;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -25,4 +26,23 @@ public class StockController {
     public List<Stock> findAllStocks(){
         return stockService.findAllStocks();
     }
+
+    @GetMapping("/findstockbyname")
+    public Stock findStockByName(@RequestParam String name) {
+        return stockService.findStockByName(name);
+    }
+
+    // add data for a stock
+    @GetMapping("/importstocks")
+    public Stock importStocks(@RequestParam String symbolInput) {
+        return stockService.importStocks(symbolInput);
+    }
+
+    // STILL DOING
+    // get 1 year difference for stock
+    // @GetMapping("/oneyeardifference")
+    // public double getOneYearDifferenceStockPrice(@RequestParam String symbolInput) {
+    //     return stockService.getOneYearDifferenceStockPrice(symbolInput);
+    // }
+
 }
---
+++ backend/src/main/java/g1t1/backend/stock/StockInstance.java
@@ -0,0 +1,65 @@
+package g1t1.backend.stock;
+
+public class StockInstance {
+   private double open;
+   private double high;
+   private double low;
+   private double close;
+   private double adjustedClose;
+   private long volume;
+   private double dividendAmount;
+   private double splitCoefficient;
+   private String dateTime;
+
+   public StockInstance(double open, double high, double low, double close, double adjustedClose, long volume, double dividendAmount, double splitCoefficient, String dateTime) {
+      this.open = open;
+      this.high = high;
+      this.low = low;
+      this.close = close;
+      this.adjustedClose = adjustedClose;
+      this.volume = volume;
+      this.dividendAmount = dividendAmount;
+      this.splitCoefficient = splitCoefficient;
+      this.dateTime = dateTime;
+   }
+
+   public double getOpen() {
+      return this.open;
+   }
+
+   public double getHigh() {
+      return this.high;
+   }
+
+   public double getLow() {
+      return this.low;
+   }
+
+   public double getClose() {
+      return this.close;
+   }
+
+   public double getAdjustedClose() {
+      return this.adjustedClose;
+   }
+
+   public long getVolume() {
+      return this.volume;
+   }
+
+   public double getDividendAmount() {
+      return this.dividendAmount;
+   }
+
+   public double getSplitCoefficient() {
+      return this.splitCoefficient;
+   }
+
+   public String getDate() {
+      return this.dateTime;
+   }
+
+   public String toString() {
+      return "\nStockUnit{open=" + this.open + ", high=" + this.high + ", low=" + this.low + ", close=" + this.close + ", adjustedClose=" + this.adjustedClose + ", volume=" + this.volume + ", dividendAmount=" + this.dividendAmount + ", splitCoefficient=" + this.splitCoefficient + ", date=" + this.dateTime + '}';
+   }
+}
